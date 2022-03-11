d0c5668adf661f4d58d1a5fb6c324bcbcc9504df
Benton J <phixmeplease@gmail.com>
2022-03-11 18:49:35 -0500

    eating dogs and cats

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-function SetStatusChanged( status ) {
-    let processNode = document.querySelector("#hello");
-    console.log(processNode);
-    processNode.innerHTML = status;
-}
+/**
+ * Texts to randomly display while loading.
+ */
+ const texts = [
+    "This map is temporary and will be replaced soon",
+    "Family guy sucks",
+    "Make sure to record the karens to put them on worldstar",
+    "Try to follow the rules, it makes you 100x cooler",
+    "People who disrespect people kinda suck",
+    "Call 911 if you like to buy drugs",
+    "Make sure to do illigal things, it's the American Way",
+    "Put it on MySpace"
+];
+
+let curText = ``;
+let curLetter = ``;
+
+let textIndex = 0;
+let letterIndex = 0;
+
+/**
+ * Type out the current text.
+ */
+const type = () => {
+    if (textIndex === texts.length) textIndex = 0;
+
+    curText = texts[textIndex];
+    curLetter = curText.slice(0, ++letterIndex);
+
+    document.querySelector(`.tips`).textContent = curLetter;
+
+    if (letterIndex === curText.length) {
+        textIndex++;
+        letterIndex = 0;
+
+        setTimeout(type, 1000);
+        return;
+    }
+
+    // var audio = new Audio('/sound/clack.ogg');
+    // audio.volume = 0.05
+    // audio.play();
+    setTimeout(type, 100);
+};
+
+// Initialize.
 window.onload = () => {
-    SetStatusChanged( "Setting Up..." )
-}
+    type();
+};
